fed5f74cb50e0ef70f330eb04c5f4a33c8c3f70a
Keenan Cheung <74540132+KeenanCheung4HA@users.noreply.github.com>
2023-01-25 09:48:30 +0000

    I still don't know ho to do pictures

--- db/create.sql
+++ db/create.sql
@@ -24,6 +24,6 @@ insert into products (name, description, code, price) values ('Attack on Titan',
 insert into products (name, description, code, price) values ('Captain Levi figure', 'Master of the way of the sword and is a titan killing God', '077030122-3', 40);
 insert into products (name, description, code, price) values ('Kakashi Sensei', 'LIGHTNING BLADE!!!!!!!', '075729122-x', 9999);
 insert into products (name, description, code, price) values ('THE One Piece', 'THE ONE PIECE IS REAL!!!! After 1043 episodes', '693155505-7', 10);
-insert into products (name, description, code, price) values ('Sasuke Mangekyo Sharingan', 'Sasuke uses his blood eye to decimate his opponents', '686928463-6', 30);
+insert into products (name, description, code, price) values ('Sasuke Mangekyo Sharingan', 'Sasuke uses his blood eyes to decimate his opponents', '686928463-6', 30);
 insert into products (name, description, code, price) values ('Goku Kaioken x20', 'Kaioken makes the body strong, but gives you a big toll on your life', '492662523-7', 20);
 
