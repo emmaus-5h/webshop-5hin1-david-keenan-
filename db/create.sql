1df16298df9d843add3240ac1697f7f5ab1045e3
Keenan Cheung <74540132+KeenanCheung4HA@users.noreply.github.com>
2023-01-25 09:33:10 +0000

    how to add pictures

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Attack on Titan', 'Vecht met titans', '816905633-0', 20.0);
-insert into products (name, description, code, price) values ('Captain Levi figure', 'TitanKiller69', '077030122-3', 40);
+insert into products (name, description, code, price) values ('Attack on Titan', 'Fighting with titans, for the freedom of Paradis', '816905633-0', 20.0);
+insert into products (name, description, code, price) values ('Captain Levi figure', 'Master of the way of the sword and is a titan killing God', '077030122-3', 40);
 insert into products (name, description, code, price) values ('Kakashi Sensei', 'LIGHTNING BLADE!!!!!!!', '075729122-x', 9999);
-insert into products (name, description, code, price) values ('THE One Piece', 'THE ONE PIECE IS REAL!!!!', '693155505-7', 10);
-insert into products (name, description, code, price) values ('Tunnel', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Denneboom', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('THE One Piece', 'THE ONE PIECE IS REAL!!!! After 1043 episodes', '693155505-7', 10);
+insert into products (name, description, code, price) values ('Sasuke Mangekyo Sharingan', 'Sasuke uses his blood eye to decimate his opponents', '686928463-6', 30);
+insert into products (name, description, code, price) values ('Goku Kaioken x20', 'Kaioken makes the body strong, but gives you a big toll on your life', '492662523-7', 20);
 
